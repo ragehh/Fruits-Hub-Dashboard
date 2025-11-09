b7f3fdf941375d45454ed165b8b549465099a59e
ragehh <mrageh68@gmail.com>
2025-11-09 09:26:53 +0200

    first commit

---
+++ lib/features/dashboard/presentation/views/dashboard_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+static const routeName = 'DashboardView';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
