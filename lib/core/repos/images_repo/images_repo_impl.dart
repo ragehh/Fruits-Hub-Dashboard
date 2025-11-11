96a9f129e4d26e368df236ad1024e7315e665ba3
ragehh <mrageh68@gmail.com>
2025-11-11 16:03:02 +0200

    Create Images and Products Repo

---
+++ lib/core/repos/images_repo/images_repo_impl.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub_dashboard/core/errors/failure.dart';
+
+import 'images_repo.dart';
+
+class ImagesRepoImpl implements ImagesRepo {
+  @override
+  Future<Either<Failure, String>> uploadImage(File image) {
+    // TODO: implement uploadImage
+    throw UnimplementedError();
+  }
+}
---
+++ lib/core/repos/products_repo/products_repo_impl.dart
@@ -0,0 +1,10 @@
+import 'package:fruits_hub_dashboard/core/repos/products_repo/products_repo.dart';
+import 'package:fruits_hub_dashboard/features/add_product/domain/entities/add_product_input_entity.dart';
+
+class ProductsRepoImpl implements ProductsRepo {
+  @override
+  Future<void> addProduct(AddProductInputEntity addProductInputEntity) {
+    // TODO: implement addProduct
+    throw UnimplementedError();
+  }
+}
