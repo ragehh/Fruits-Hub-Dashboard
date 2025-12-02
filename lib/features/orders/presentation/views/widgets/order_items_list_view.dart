4dce443cd28cf1ccbd32a38aed5d1bf3ebbb48b0
ragehh <mrageh68@gmail.com>
2025-12-02 15:22:28 +0200

    Create Order Items List View

---
+++ lib/features/orders/presentation/views/widgets/order_items_list_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/features/orders/data/models/order_model.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/order_item_widget.dart';
+
+class OrderItemsListView extends StatelessWidget {
+  const OrderItemsListView({super.key, required this.orderItems});
+
+  final List<OrderModel> orderItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: orderItems.length,
+      itemBuilder: (context, index) {
+        return OrderItemWidget(orderModel: orderItems[index]);
+      },
+    );
+  }
+}
--- lib/features/orders/presentation/views/widgets/orders_view_body.dart
+++ lib/features/orders/presentation/views/widgets/orders_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/core/helper_functions/get_dummy_order_data.dart';
 import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/filter_section.dart';
-import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/order_item_widget.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/order_items_list_view.dart';
 
 class OrdersViewBody extends StatelessWidget {
   const OrdersViewBody({super.key});
@@ -15,7 +15,16 @@ class OrdersViewBody extends StatelessWidget {
           const SizedBox(height: 16),
           const FilterSection(),
           const SizedBox(height: 16),
-          OrderItemWidget(orderModel: getDummyOrder()),
+          Expanded(
+            child: OrderItemsListView(
+              orderItems: [
+                getDummyOrder(),
+                getDummyOrder(),
+                getDummyOrder(),
+                getDummyOrder(),
+              ],
+            ),
+          ),
         ],
       ),
     );
