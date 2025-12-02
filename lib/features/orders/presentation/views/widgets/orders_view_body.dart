08d1a46c9d51e5ec81dbedd008e261472de8f90f
ragehh <mrageh68@gmail.com>
2025-12-02 15:17:11 +0200

    Create Order Item Widget

--- lib/features/orders/presentation/views/widgets/orders_view_body.dart
+++ lib/features/orders/presentation/views/widgets/orders_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/core/helper_functions/get_dummy_order_data.dart';
 import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/filter_section.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/order_item_widget.dart';
 
 class OrdersViewBody extends StatelessWidget {
   const OrdersViewBody({super.key});
@@ -8,7 +10,14 @@ class OrdersViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: const Column(children: [SizedBox(height: 16), FilterSection()]),
+      child: Column(
+        children: [
+          const SizedBox(height: 16),
+          const FilterSection(),
+          const SizedBox(height: 16),
+          OrderItemWidget(orderModel: getDummyOrder()),
+        ],
+      ),
     );
   }
 }
